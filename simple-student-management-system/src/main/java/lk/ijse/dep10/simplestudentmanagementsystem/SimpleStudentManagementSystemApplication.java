25b971a83bbce5b2644ff9d9f218afde1969c540
tharuka-dilshan <tharukadk329@gmail.com>
2023-06-29 19:01:06 +0530

    set up springboot backend

---
+++ simple-student-management-system/src/main/java/lk/ijse/dep10/simplestudentmanagementsystem/SimpleStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.simplestudentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStudentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleStudentManagementSystemApplication.class, args);
+	}
+
+}
---
+++ simple-student-management-system/src/test/java/lk/ijse/dep10/simplestudentmanagementsystem/SimpleStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.simplestudentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleStudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
