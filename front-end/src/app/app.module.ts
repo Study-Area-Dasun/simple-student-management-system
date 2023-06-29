d7d12e6829497cd6747685d90fd479be8bf1089a
Buddhi Sankalpa <bsankalpa500@gmail.com>
2023-06-29 20:24:47 +0530

    implement manage-student ui

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ManageStudentComponent } from './view/manage-student/manage-student.component';
+import { ManageCoursesComponent } from './view/manage-courses/manage-courses.component';
+import { HomeComponent } from './view/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageStudentComponent,
+    ManageCoursesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
