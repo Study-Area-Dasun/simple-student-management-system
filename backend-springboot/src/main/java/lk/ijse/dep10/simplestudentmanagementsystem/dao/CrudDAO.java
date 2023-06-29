e4406f06088a0571164b199139ea304287a5fdd0
tharuka-dilshan <tharukadk329@gmail.com>
2023-06-29 20:37:59 +0530

    implement studentDAOImpl

--- backend-springboot/src/main/java/lk/ijse/dep10/simplestudentmanagementsystem/dao/CrudDAO.java
+++ backend-springboot/src/main/java/lk/ijse/dep10/simplestudentmanagementsystem/dao/CrudDAO.java
@@ -9,17 +9,9 @@ import java.util.Optional;
 
 public interface CrudDAO<T extends SuperEntity, ID extends Serializable> {
 
-    long count() throws Exception;
-
     T save(T entity) throws Exception;
-
-    void update(T entity) throws Exception;
 
     void deleteById(ID pk) throws Exception;
 
-    Optional<T> findById(ID pk) throws Exception;
-
     List<T> findAll() throws Exception;
-
-    boolean existsById(ID pk) throws Exception;
 }
--- backend-springboot/src/main/java/lk/ijse/dep10/simplestudentmanagementsystem/dao/custom/impl/StudentDAOImpl.java
+++ backend-springboot/src/main/java/lk/ijse/dep10/simplestudentmanagementsystem/dao/custom/impl/StudentDAOImpl.java
@@ -1,4 +1,49 @@
 package lk.ijse.dep10.simplestudentmanagementsystem.dao.custom.impl;
 
-public class StudentDAOImpl {
+import lk.ijse.dep10.simplestudentmanagementsystem.dao.custom.StudentDAO;
+import lk.ijse.dep10.simplestudentmanagementsystem.entity.Student;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+import static  lk.ijse.dep10.simplestudentmanagementsystem.dao.util.Mappers.STUDENT_ROW_MAPPER;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public StudentDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Student save(Student student) throws Exception {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement stm = con.prepareStatement("INSERT INTO student (name, address) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, student.getName());
+            stm.setString(2, student.getAddress());
+            return stm;
+        }, keyHolder);
+        student.setId(keyHolder.getKey().intValue());
+        return student;
+    }
+
+    @Override
+    public void deleteById(Integer pk) throws Exception {
+        jdbcTemplate.update("DELETE FROM student WHERE id=?", pk);
+    }
+
+    @Override
+    public List<Student> findAll() throws Exception {
+        return jdbcTemplate.query("SELECT * FROM student", STUDENT_ROW_MAPPER);
+    }
+
 }
