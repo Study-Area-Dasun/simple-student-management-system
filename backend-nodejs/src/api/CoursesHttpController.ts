6ceafd2ee530e70b45a7516f652322dfbe2730c7
Dasun Tharaka <wkdasunt0001@gmail.com>
2023-06-29 21:12:54 +0530

    finalized post method

--- backend-nodejs/src/api/CoursesHttpController.ts
+++ backend-nodejs/src/api/CoursesHttpController.ts
@@ -4,23 +4,43 @@ import {datasource as pool} from "../db/dbcp";
 
 type Courses={
 
-    "id":String,
-    "describtion":String,
-    "date":String
+    "id":string,
+    "description":string,
+    "duration":string
 }
 
 
 export const router = express.Router();
 
 
-router.delete('/:id',  (req, res) => {
-    console.log("hri");
-    res.send("ok")
+router.delete('/:id',  async (req, res) => {
+
+
 });
 
-router.post('/',  (req, res) => {
-    console.log("req");
-    res.send("hri")
+router.post('/',  async (req, res) => {
+    const course = (req.body as Courses);
+    console.log(course)
+    if (!course.id?.trim()){
+        res.status(400).json("invalid id");
+        return;
+    }
+   else  if (!course.description?.trim()) {
+        res.status(400).json("invalid description");
+        return;
+    } else if(!course.duration?.trim()){
+        res.status(400).json("invalid duration");
+        return;
+    }
+   try {
+       const result = await pool.query('INSERT INTO course (id,description,duration) VALUE (?,?,?)',
+           [course.id,course.description,course.duration]);
+       res.status(201).json(course);
+   }catch (e) {
+       res.status(400).json("This Id already save,Please try again!");
+   }
+
+
 });
 router.get('/',  async (req, res) => {
     const task = await pool.query('SELECT * FROM course');
